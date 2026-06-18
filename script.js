4c32812b68a25edfd4e8f8bd61a8d11d2a81be1e
Felipes-A <felipeasmariano19@gmail.com>
2026-06-18 14:31:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const musica = document.getElementById("musica");
 const btnMusica = document.getElementById("btnMusica");
 const meuVideo = document.getElementById("meu-video");
-meuVideo.pause();
+meuVideo.pause("musica");
 
 btnMusica.addEventListener("click", () => {
     if (musica.paused) {
